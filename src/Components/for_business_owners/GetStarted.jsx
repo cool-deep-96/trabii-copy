ff4e03828da5a1643d5017960e38987348c822eb
cool-deep-96 <27581kuldeep.2021cse@gmail.com>
2024-10-26 07:37:22 +0530

    on going

--- src/Components/for_business_owners/GetStarted.jsx
+++ src/Components/for_business_owners/GetStarted.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GetStarted = () => {
   return (
     <div className="w-full flex justify-center bg-black">
-      <div className="max-w-[355px] lg:min-w-max lg:max-w-screen-xl px-5  pb-16 lg:pb-24 pt-10 lg:pt-24 flex flex-col items-center gap-16 lg:gap-24">
+      <div className="max-w-[355px] w-full lg:max-w-screen-xl px-5  pb-16 lg:pb-24 pt-10 lg:pt-24 flex flex-col items-center gap-16 lg:gap-24">
         <p className="text-white text-center w-full font-bold text-4xl lg:text-[3.5rem]">
           Contact Form for Business Owners
         </p>
--- src/Components/for_business_owners/Short.jsx
+++ src/Components/for_business_owners/Short.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import smile from '../../assets/for_business_owners/smile.png'
+import React from "react";
+import smile from "../../assets/for_business_owners/smile.png";
 
 const Short = () => {
   return (
-    <div className='w-full flex justify-center bg-black'>
-        <div className='relative max-w-[355px] w-full lg:max-w-screen-lg px-5 pt-10 lg:pt-20 lg:pl-40 pb-32  flex items-start  '>
-            <img src={smile} alt="smile.png" className='absolute h-[40%] lg:h-[65%]  w-auto opacity-30 bottom-40 lg:bottom-24 right-5' />
-            <p className='relative lg:w-[50%] text-3xl lg:text-4xl font-bold text-white'>At <span className='text-yellow-300'>TraBii</span>, we believe in simplicity and fairness when it comes to pricing. Our transparent pricing model ensures that you know exactly what to expect.
-            </p>
-        </div>
-
+    <div className="w-full flex justify-center bg-black">
+      <div className="relative max-w-[355px] w-full lg:max-w-screen-lg px-5 pt-10 lg:pt-20 lg:pl-40 pb-32  flex items-start  ">
+        <img
+          src={smile}
+          alt="smile.png"
+          className="absolute h-[40%] lg:h-[65%]  w-auto opacity-30 bottom-40 lg:bottom-24 right-5"
+        />
+        <p className="relative lg:w-[50%] text-3xl lg:text-4xl font-bold text-white">
+          At <span className="text-yellow-300">TraBii</span>, we believe in
+          simplicity and fairness when it comes to pricing. Our transparent
+          pricing model ensures that you know exactly what to expect.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Short
+export default Short;
