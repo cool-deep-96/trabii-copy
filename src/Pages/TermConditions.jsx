2c56a22fac951d5a13396efb71540d9d00f0cf3c
Atul Kumar <atulshahi2003@gmail.com>
2024-10-26 12:42:56 +0530

    legality pagecomplete

---
+++ src/Pages/TermConditions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./policy.css";
+
+const TermConditions = () => {
+  return (
+    <div className="w-full flex justify-center py-3">
+      <div className="max-w-[355px] lg:max-w-screen-xl">
+        <div className="h-96 bg-white flex justify-center py-40">
+            <p className="text-center font-bold text-3xl ">Will be available soon</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TermConditions;;
